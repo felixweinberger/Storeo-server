511867ec396c015d4d3fb634d93483c368ab5fb1
Felix Weinberger <felix.weinberger@gmail.com>
2019-03-12 10:23:03 +0100

    fix(auth): increase resilience of auth_server -> able to handle db down

--- auth_svc/routes/userImports.js
+++ auth_svc/routes/userImports.js
@@ -1,9 +1,7 @@
 import signup from '../Controllers/signup';
 import login from '../Controllers/login';
-import updateAddress from '../Controllers/updateAddress';
 
 export default {
   signup,
   login,
-  updateAddress,
 };
--- auth_svc/routes/user_routes.js
+++ auth_svc/routes/user_routes.js
@@ -1,7 +1,5 @@
 import express from 'express';
 import user from './userImports';
-import authMiddleware from '../Middlewares/authorization';
-import gateMiddleware from '../Middlewares/gate';
 
 const router = express.Router();
 
@@ -10,8 +8,5 @@ router.post('/signup', user.signup);
 
 // Login
 router.get('/login', user.login);
-
-// Add Address
-router.post('/address', authMiddleware, gateMiddleware, user.updateAddress);
 
 module.exports = router;
